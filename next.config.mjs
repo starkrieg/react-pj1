6e727e667d347a20191388470c7dcaa5cc5bcc76
starkrieg <93294578+starkrieg@users.noreply.github.com>
2024-10-03 21:41:29 -0300

    fix: adjustments to publish

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
     basePath: "/react-pj1", // <== base path for assets
     output: "export", // <=== enables static exports
-    reactStrictMode: true
+    /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
