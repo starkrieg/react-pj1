989f7cd5688d8f10b91984baa47940458848a547
starkrieg <93294578+starkrieg@users.noreply.github.com>
2025-01-31 19:18:12 -0300

    bugfix: update version in properties

--- src/app/properties/properties.js
+++ src/app/properties/properties.js
@@ -1,3 +1,3 @@
-export const appVersion = '0.4.5 - alpha';
+export const appVersion = '0.4.6 - alpha';
 export const saveFileVersion = '0.1.0'
 export const storage_save_key = 'journey-beyond-save-data'
